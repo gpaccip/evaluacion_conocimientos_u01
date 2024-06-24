30420ec6e22ce5a393e2adad615558064c95f053
gpaccip <gpaccip@unjbg.edu.pe>
2024-06-23 23:59:24 -0500

    Actualizacion del ejercicio3: añadiendo mensajes visuales

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -3,23 +3,25 @@ using namespace std;
 float suma(int);
 int factorial(int);
 int main(){
-    int n;
+    int N;
     float sum;
-		cin>>n;
-    	sum=suma(n);
-		cout<<sum;
+		cout<<"Digite un numero: ";cin>>N;
+    		sum=suma(N);
+		cout<<"La suma es: "<<sum;
 }
 float suma(int a){
-    float s=0;
-    	for(int i=1;i<=a;i++){
-       		s=s+(float(factorial(i))/(2*i));
+    float s;
+    	s=0;
+    		for(int i=1;i<=a;i++){
+       			s=s+(float(factorial(i))/(2*i));
     }
     return s;
 }
 int factorial(int b){
-    int f=1;
-    	for(int i=2;i<=b;i++){
-        	f=f*i;
+    int f;
+    	f=1;
+    		for(int i=2;i<=b;i++){
+        		f=f*i;
     }
 	return f;
 }
