3facd1b07fed1ef2957b686e3b6598abda7306cb
gpaccip <gpaccip@unjbg.edu.pe>
2024-06-23 22:53:03 -0500

    Actualizacion del ejercicio1: añadiendo mensajes visuales y se añade una validacion del operador

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -3,19 +3,21 @@ using namespace std;
 int main(){
 	double op1, op2, r;
 	char op;
-		cin>>op1;
-		cin>>op2;
-		cin>>op;
+		cout<<"\t CALCULADORA"<<endl;
+		cout<<"Digite el primer numero: ";cin>>op1;
+		cout<<"Digite el segundo numero: ";cin>>op2;
+		cout<<"Digite el operador: ";cin>>op;
 	
 	switch(op){
 	case '+': r=op1+op2;
-				cout<<r;break;
+				cout<<"El resultado es: "<<r;break;
 	case '-': r=op1-op2;
-				cout<<r;break;		
+				cout<<"El resultado es: "<<r;break;		
 	case '*': r=op1*op2;
-				cout<<r;break;
+				cout<<"El resultado es: "<<r;break;
 	case '/': r=op1/op2;
-				cout<<r;break;	
+				cout<<"El resultado es: "<<r;break;	
+	default: cout<<"El operador es erroneo ";		
 	}
 	
 	return 0;
