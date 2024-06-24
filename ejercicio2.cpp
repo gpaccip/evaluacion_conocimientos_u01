e0df3b70fd7886c7d8a2ddc6fcde20fc23a1a09f
gpaccip <gpaccip@unjbg.edu.pe>
2024-06-23 23:35:52 -0500

    Actualizacion del ejercicio2: añadiendo mensajes visuales y arreglando una variable

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 int main(){
     int n1,n2,c=0, i=1;
-    cin>>n1;
-    cin>>n2;
-    while(i<=n1&&i<=2){
+		cout<<"\tDETERMINANDO SI DOS NUMEROS SON PESSI O NO"<<endl;
+		cout<<"Digite un numero: ";cin>>n1;
+		cout<<"Digite otro numero: ";cin>>n2;
+    while(i<=n1&&i<=n2){
         if(n1%i==0&&n2%i==0){
             c++;
         }
